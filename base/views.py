d2b6bfd5edf88136ff3f353fe4537c141075e2aa
Adrian <adrian.adam.zaleski@gmail.com>
2022-01-04 18:19:09 +0100

    Prepared templates from static version to django version

--- base/views.py
+++ base/views.py
@@ -3,11 +3,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
     return render(request, 'base/index.html')
 
 def posts(request):
-    return render(request, 'base/posts.html')
+    
+    posts= [
+        {
+            'headline': 'To Do app',
+            'sub_headline': ' First app made. Simple to do app.',
+        },
+        
+        {
+            'headline': 'Coś tam coś tam',
+            'sub_headline': 'Some text here. Need more text.Some text here. Need more text.Some text here. Need more text.Some text here. Need more text.Some text here. Need more text.',
+        },
+        
+        {
+            'headline': 'Need of Title',
+            'sub_headline': 'KOlejna partia tekstu tutaj',
+        },
+    ]
+    context = {'posts': posts}
+    return render(request, 'base/posts.html', context)
 
 def post(request):
     return render(request, 'base/post.html')
